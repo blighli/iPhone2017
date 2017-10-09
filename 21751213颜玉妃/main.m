cd47c2630e4fa6d08c18746df78140198e1ea56b
YanYufei <yanyufei@hyperchain.cn>
2017-10-09 21:19:53 +0800

    modify main.m 入参个数

--- 21751213颜玉妃/main.m
+++ 21751213颜玉妃/main.m
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[]) {
         PrintYearAndMonth *printMonthObj = [[PrintYearAndMonth alloc] init];
     
         
-        if (argc == 2) {
+        if (argc == 1) {
             //执行“cal”命令；
             //执行该命令时argc = 2;argv[1] = cal;argv[2] = null
             
@@ -31,18 +31,18 @@ int main(int argc, const char * argv[]) {
             
             [printMonthObj printMonth:year yue:yue];
 //            NSLog(@"1111");
-        }else if(argc == 3){
+        }else if(argc == 2){
             //执行“cal 2014”命令；
             //这个argv2表示年份；
-            NSString *argv2 = [NSString stringWithUTF8String:argv[2]];
+            NSString *argv2 = [NSString stringWithUTF8String:argv[1]];
             
             [printYearObj printYear:[argv2 intValue]];
 //            NSLog(@"2222");
-        }else if(argc == 4){
+        }else if(argc == 3){
             //执行“cal 10 2014”命令；
             //这一步是为了把传递进来的char * argv参数转化为 OC中的NSString；
-            NSString *argv2 = [NSString stringWithUTF8String:argv[2]];
-            NSString *argv3 = [NSString stringWithUTF8String:argv[3]];
+            NSString *argv2 = [NSString stringWithUTF8String:argv[1]];
+            NSString *argv3 = [NSString stringWithUTF8String:argv[2]];
             [printMonthObj printMonth:[argv3 intValue] yue:[argv2 intValue]];
             //NSLog(@"44444");
         }
