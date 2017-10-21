a650c2f27f0a9e5ff6116075cc42b3a29cb7a9b6
xuting <xuting_ahaq@163.com>
2017-10-21 21:31:09 +0800

    submit project02

---
+++ 21751118许婷/project02/myTaskList/myTaskListTests/myTaskListTests.m
@@ -0,0 +1,39 @@
+//
+//  myTaskListTests.m
+//  myTaskListTests
+//
+//  Created by ting xu on 2017/10/21.
+//  Copyright © 2017年 ting xu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface myTaskListTests : XCTestCase
+
+@end
+
+@implementation myTaskListTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
