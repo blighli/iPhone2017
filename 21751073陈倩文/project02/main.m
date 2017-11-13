4eb5bfc3971c7bb25147bc88bfd4fca45c3e70ad
cutewen <549421908@qq.com>
2017-11-13 14:58:16 +0800

    homework2

---
+++ 21751073陈倩文/project02/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cqwTaskList
+//
+//  Created by 陈倩文 on 2017/10/23.
+//  Copyright © 2017年 chenqianwen. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
